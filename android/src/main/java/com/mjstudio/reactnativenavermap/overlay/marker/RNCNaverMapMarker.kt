b982f1537f0af0dbad2e67f82ba0ce9b47057a73
mym0404 <mym0404@gmail.com>
2024-04-07 22:59:50 +0900

    chore: remove reproducing sample field in bug report

--- android/src/main/java/com/mjstudio/reactnativenavermap/overlay/marker/RNCNaverMapMarker.kt
+++ android/src/main/java/com/mjstudio/reactnativenavermap/overlay/marker/RNCNaverMapMarker.kt
@@ -1,10 +1,12 @@
 package com.mjstudio.reactnativenavermap.overlay.marker
 
+import android.annotation.SuppressLint
 import com.facebook.react.uimanager.ThemedReactContext
 import com.mjstudio.reactnativenavermap.overlay.RNCNaverMapOverlay
 import com.naver.maps.map.NaverMap
 import com.naver.maps.map.overlay.Marker
 
+@SuppressLint("ViewConstructor")
 class RNCNaverMapMarker(val reactContext: ThemedReactContext) :
     RNCNaverMapOverlay<Marker>(reactContext) {
     override val overlay: Marker by lazy {
