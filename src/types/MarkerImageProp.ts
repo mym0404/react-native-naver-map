766f04b08165deab43a55ded3f941321b9c58369
mym0404 <mym0404@gmail.com>
2024-04-18 00:37:45 +0900

    docs: add docs

--- src/types/MarkerImageProp.ts
+++ src/types/MarkerImageProp.ts
@@ -1,6 +1,9 @@
 import type { MarkerSymbol } from './MarkerSymbol';
 import type { ImageRequireSource } from 'react-native/Libraries/Image/ImageSource';
 
+/**
+ * 마커의 이미지 Prop 타입입니다.
+ */
 export type MarkerImageProp =
   | ImageRequireSource
   | {
