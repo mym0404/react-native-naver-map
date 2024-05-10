6ca975c0f000243ecb0295dbb931410544d83441
mym0404 <mym0404@gmail.com>
2024-05-10 23:37:26 +0900

    fix(ios): remove util function

--- ios/Module/RNCNaverMapUtil.mm
+++ ios/Module/RNCNaverMapUtil.mm
@@ -7,34 +7,10 @@
 
 #import "RNCNaverMapUtil.h"
 #import <Foundation/Foundation.h>
-#import <string>
 
 @implementation RNCNaverMapUtil
 
 RCT_EXPORT_MODULE()
-
-RCT_EXPORT_METHOD(setGlobalZIndex : (NSString*)type zIndex : (double)zIndex) {
-  std::string t = [type UTF8String];
-  if (t == "InfoWindow") {
-    [NMFInfoWindow new].zIndex = zIndex;
-  } else if (t == "Location") {
-    [NMFLocationOverlay new].globalZIndex = zIndex;
-  } else if (t == "Marker") {
-    [NMFMarker new].globalZIndex = zIndex;
-  } else if (t == "ArrowheadPath") {
-    [NMFArrowheadPath new].globalZIndex = zIndex;
-  } else if (t == "Path") {
-    [NMFPath new].globalZIndex = zIndex;
-  } else if (t == "Polygon") {
-    [NMFPolygonOverlay new].globalZIndex = zIndex;
-  } else if (t == "Polyline") {
-    [NMFPolylineOverlay new].globalZIndex = zIndex;
-  } else if (t == "Circle") {
-    [NMFCircleOverlay new].globalZIndex = zIndex;
-  } else if (t == "Ground") {
-    [NMFGroundOverlay new].globalZIndex = zIndex;
-  }
-}
 
 #ifdef RCT_NEW_ARCH_ENABLED
 - (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:
