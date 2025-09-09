3c00b42a23eaf2a634249938b5848884a87991f2
MJ <mym0404@gmail.com>
2025-09-09 22:12:25 +0900

    docs: add fumadocs as doc2

---
+++ docs2/src/lib/source.ts
@@ -0,0 +1,18 @@
+import {docs} from '@/.source';
+import {loader} from 'fumadocs-core/source';
+import {createElement} from "react";
+import { icons} from 'lucide-react'
+
+// See https://fumadocs.vercel.app/docs/headless/source-api for more info
+export const source = loader({
+  // it assigns a URL to your pages
+  baseUrl: '/docs',
+  source: docs.toFumadocsSource(),
+  icon(icon) {
+    if (!icon) {
+      // You may set a default icon
+      return;
+    }
+    if (icon in icons) return createElement(icons[icon as keyof typeof icons]);
+  },
+});
