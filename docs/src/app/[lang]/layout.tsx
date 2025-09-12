552cbb7e1636d1e31402f85ee0da467b22db7b42
MJ <mym0404@gmail.com>
2025-09-13 01:09:10 +0900

    chore: unconscious commit 🎲

--- docs/src/app/[lang]/layout.tsx
+++ docs/src/app/[lang]/layout.tsx
@@ -91,7 +91,12 @@ export default async function Layout({
   return (
     <html lang={lang} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
-        <RootProvider i18n={provider(lang)}>{children}</RootProvider>
+        <RootProvider
+          i18n={provider(lang)}
+          theme={{ defaultTheme: 'dark', enableSystem: false }}
+        >
+          {children}
+        </RootProvider>
       </body>
     </html>
   );
